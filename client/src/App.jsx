305a7cf42bd0fe479ceff2d8ffa56dc88130482c
Suryansh Sharma <suryansharmaa@gmail.com>
2025-07-17 10:04:59 +0530

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">Hello Tailwind + Shadcn!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
