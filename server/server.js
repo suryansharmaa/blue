9e7c3062fdea58ce049e39af37d67e1ad1799af5
Suryansh Sharma <suryansharmaa@gmail.com>
2025-07-28 17:21:17 +0530

    backend CRUD

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const postRoutes = require("./routes/postRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/posts", postRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from server!");
@@ -11,3 +18,14 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("DB connection error : ", error.message);
+  }
+};
+
+startServer();
